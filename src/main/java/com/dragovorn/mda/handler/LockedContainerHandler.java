a6c68dbfcdee7c155ef6ab52bc88b771a9a744c3
Dragovorn <me@dragovorn.com>
2017-11-29 17:10:51 -0500

    make sure doors can be locked too

--- src/main/java/com/dragovorn/mda/handler/LockedContainerHandler.java
+++ src/main/java/com/dragovorn/mda/handler/LockedContainerHandler.java
@@ -7,12 +7,18 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.material.Openable;
 
 public class LockedContainerHandler implements Listener {
 
     @EventHandler
     public void checkInteraction(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) { // Make sure a block is being interacted with
+            return;
+        }
+
+        if (!(event.getClickedBlock().getState() instanceof InventoryHolder || event.getClickedBlock().getState().getData() instanceof Openable)) { // Make sure a block that holds an inventory or is openable is being interacted with
             return;
         }
 
--- src/test/java/com/dragovorn/mda/handler/LockedContainerHandlerTest.java
+++ src/test/java/com/dragovorn/mda/handler/LockedContainerHandlerTest.java
@@ -3,9 +3,12 @@ package com.dragovorn.mda.handler;
 import com.dragovorn.mda.Main;
 import com.dragovorn.mda.manager.ILockManager;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Door;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -21,7 +24,7 @@ import static org.powermock.api.mockito.PowerMockito.*;
 public class LockedContainerHandlerTest {
 
     @Test
-    public void testLocked() {
+    public void testLockedChest() {
         Player owner = mock(Player.class);
         when(owner.getName()).thenReturn("Owner");
 
@@ -37,10 +40,48 @@ public class LockedContainerHandlerTest {
 
         Player clicked = mock(Player.class);
 
+        Block chest = mock(Block.class);
+        when(chest.getState()).thenReturn(mock(Chest.class));
+
         PlayerInteractEvent interactEvent = mock(PlayerInteractEvent.class);
         when(interactEvent.getAction()).thenReturn(Action.RIGHT_CLICK_BLOCK);
         when(interactEvent.getPlayer()).thenReturn(clicked);
-        when(interactEvent.getClickedBlock()).thenReturn(mock(Block.class));
+        when(interactEvent.getClickedBlock()).thenReturn(chest);
+
+        LockedContainerHandler handler = new LockedContainerHandler();
+        handler.checkInteraction(interactEvent);
+
+        verify(clicked, times(1)).sendMessage("This chest is locked by: Owner!");
+        verify(interactEvent, times(1)).setCancelled(true);
+    }
+
+    @Test
+    public void testLockedDoor() {
+        Player owner = mock(Player.class);
+        when(owner.getName()).thenReturn("Owner");
+
+        ILockManager manager = mock(ILockManager.class);
+        when(manager.isLocked(any(Block.class))).thenReturn(true);
+        when(manager.getWhoLocked(any(Block.class))).thenReturn(owner);
+
+        Main main = mock(Main.class);
+        when(main.getLockManager()).thenReturn(manager);
+
+        mockStatic(Main.class);
+        when(Main.getInstance()).thenReturn(main);
+
+        Player clicked = mock(Player.class);
+
+        BlockState state = mock(BlockState.class);
+        when(state.getData()).thenReturn(mock(Door.class));
+
+        Block door = mock(Block.class);
+        when(door.getState()).thenReturn(state);
+
+        PlayerInteractEvent interactEvent = mock(PlayerInteractEvent.class);
+        when(interactEvent.getAction()).thenReturn(Action.RIGHT_CLICK_BLOCK);
+        when(interactEvent.getPlayer()).thenReturn(clicked);
+        when(interactEvent.getClickedBlock()).thenReturn(door);
 
         LockedContainerHandler handler = new LockedContainerHandler();
         handler.checkInteraction(interactEvent);
